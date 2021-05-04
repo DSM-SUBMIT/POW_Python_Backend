040cc0ec89a259110f8eac98631088abe23f4335
kwakdony <kwakdh25@gmail.com>
2021-05-04 12:38:38 +0900

    ?

--- submit_pow/club_project/urls.py
+++ submit_pow/club_project/urls.py
@@ -4,7 +4,7 @@ from club_project import views
 app_name = 'club'
 
 urlpatterns = [
-    path('club/<int:club_id>/project', views.ProjectListAPIView.as_view()),
-    path('club/<int:club_id>/project/<int:project_id>', views.ProjectDetailAPIView.as_view()),
+    path('club/<int:club_id>/project/', views.ProjectAPIView.as_view(), name='project'),
+    path('club/<int:club_id>/project/<int:project_id>', views.ProjectDetailAPIView.as_view(), name='project_detail'),
     path('club/<int:club_id>', views.ClubIntroUpdateAPIView.as_view(), name='update_club_intro'),
 ]
--- submit_pow/club_project/views.py
+++ submit_pow/club_project/views.py
@@ -1,9 +1,11 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
+from rest_framework_jwt.authentication import JSONWebTokenAuthentication
 from django.shortcuts import get_object_or_404
 
 from club_project import serializers
+from club_project.permissions import IsOwnerOrReadOnly
 from club_project.models import TblProjectIntroduction, TblClub
 
 from submit_pow import settings
@@ -11,22 +13,27 @@ from submit_pow import settings
 import jwt
 
 
-class ProjectListAPIView(APIView):
-
+class ProjectAPIView(APIView):
     serializer_class = serializers.ProjectSerializer
+    authentication_classes = [JSONWebTokenAuthentication]
+    permission_classes = (IsOwnerOrReadOnly,)
+
+    def verify_authentication_token(self, request, club_id):
+        token = jwt.decode(request.auth, settings.SECRET_KEY, algorithms=["HS256"])
+        # {'user_id': 1, 'username': 'kwak', 'exp': 1619447947, 'email': 'kwak@kwak.com', 'orig_iat': 1618843147}
+        return token['club_id'] != club_id
 
     def get(self, request, club_id):
-        serializer = self.serializer_class(TblProjectIntroduction.objects.filter(club_id=club_id), many=True)
+        queryset = TblProjectIntroduction.objects.filter(club_id=club_id)
+        serializer = self.serializer_class(queryset, many=True)
+
         return Response(serializer.data)
 
     def post(self, request, club_id):
         """Create a project object"""
-        token = jwt.decode(request.auth, settings.SECRET_KEY, algorithms=["HS256"])
-        # {'user_id': 1, 'username': 'kwak', 'exp': 1619447947, 'email': 'kwak@kwak.com', 'orig_iat': 1618843147}
-        if token['club_id'] != club_id:
+        if self.verify_authentication_token(request, club_id):
             return Response(status=status.HTTP_401_UNAUTHORIZED)
 
-        request.data['club_id'] = club_id
         serializer = self.serializer_class(data=request.data)
 
         if serializer.is_valid():
@@ -40,10 +47,13 @@ class ProjectListAPIView(APIView):
 
 
 class ProjectDetailAPIView(APIView):
-
     serializer_class = serializers.ProjectSerializer
 
-    # noinspection PyMethodMayBeStatic
+    def verify_authentication_token(self, request, club_id):
+        # {'user_id': 1, 'username': 'kwak', 'exp': 1619447947, 'email': 'kwak@kwak.com', 'orig_iat': 1618843147}
+        token = jwt.decode(request.auth, settings.SECRET_KEY, algorithms=["HS256"])
+        return token['club_id'] != club_id
+
     def get_object(self, project_id):
         return get_object_or_404(TblProjectIntroduction, pk=project_id)
 
@@ -53,11 +63,18 @@ class ProjectDetailAPIView(APIView):
         serializer = self.serializer_class(project)
         return Response(serializer.data)
 
+
     def put(self, request, club_id, project_id):
         """Update a project object"""
+        if self.verify_authentication_token(request, club_id):
+            return Response(
+                {"error_message": "You don't have permission to update"},
+                status=status.HTTP_401_UNAUTHORIZED
+            )
+
         project = self.get_object(project_id)
-        request.data['club_id'] = club_id
         serializer = self.serializer_class(project, data=request.data)
+
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
@@ -66,8 +83,13 @@ class ProjectDetailAPIView(APIView):
             status=status.HTTP_400_BAD_REQUEST
         )
 
-    def delete(self, request, project_id):
+    def delete(self, request, club_id, project_id):
         """Delete a project object"""
+        if self.verify_authentication_token(request, club_id):
+            return Response(
+                {"error_message": "You don't have permission to delete"},
+                status=status.HTTP_401_UNAUTHORIZED
+            )
         project = self.get_object(project_id)
         project.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
@@ -75,12 +97,44 @@ class ProjectDetailAPIView(APIView):
 
 class ClubIntroUpdateAPIView(APIView):
 
-    def put(self, request, club_id):
-        """Update a club introduction(contents)"""
-        club = get_object_or_404(TblClub.objects.filter(id=club_id))
-        serializer = serializers.ClubSerializer(club, data=request.data)
+    serializer_class = serializers.ClubSerializer
+
+    def verify_authentication_token(self, request, club_id):
+        # {'user_id': 1, 'username': 'kwak', 'exp': 1619447947, 'email': 'kwak@kwak.com', 'orig_iat': 1618843147}
+        token = jwt.decode(request.auth, settings.SECRET_KEY, algorithms=["HS256"])
+        return token['club_id'] != club_id
+
+    def get(self, request, club_id):
+        club = get_object_or_404(TblClub, pk=club_id)
+        serializer = self.serializer_class(club)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request, club_id):
+        if self.verify_authentication_token(request, club_id):
+            return Response(
+                {"error_message": "You don't have permission to create"},
+                status=status.HTTP_401_UNAUTHORIZED
+            )
+
+        serializer = serializers.ClubSerializer(data=request.data)
+
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_200_OK)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+    def put(self, request, club_id):
+        """Update a club introduction(contents)"""
+        if self.verify_authentication_token(request, club_id):
+            return Response(
+                {"error_message": "You don't have permission to update"},
+                status=status.HTTP_401_UNAUTHORIZED
+            )
+
+        club = get_object_or_404(TblClub.objects.filter(id=club_id))
+        serializer = serializers.ClubSerializer(club, data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
