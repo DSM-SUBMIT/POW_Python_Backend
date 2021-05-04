a6c368fbf7471842520c95d6bc42cd4681853d5d
kwakdony <kwakdh25@gmail.com>
2021-05-04 16:59:16 +0900

    init

--- submit_pow/settings.py
+++ submit_pow/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 import os
 import datetime
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -155,3 +156,6 @@ STATIC_URL = '/static/'
 STATIC_ROOT = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
