abe45ad6ce756ffb67c13cc9fc8023f27ec9dcef
kwakdony <kwakdh25@gmail.com>
2021-05-05 20:07:49 +0900

    ADD admin.py

--- club_project/admin.py
+++ club_project/admin.py
@@ -1,6 +1,6 @@
-# from django.contrib import admin
-# from club_project.models import Project
-#
-# @admin.register(Project)
-# class ProjectAdmin(admin.ModelAdmin):
-#     list_display = ('id', 'title', 'contents', 'started_at', 'ended_at')
+from django.contrib import admin
+from club_project.models import TblProjectIntroduction
+
+@admin.register(TblProjectIntroduction)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'contents', 'started_at', 'ended_at')
---
+++ models.py
@@ -0,0 +1,184 @@
+# This is an auto-generated Django model module.
+# You'll have to do the following manually to clean this up:
+#   * Rearrange models' order
+#   * Make sure each model has one field with primary_key=True
+#   * Make sure each ForeignKey and OneToOneField has `on_delete` set to the desired behavior
+#   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
+# Feel free to rename the models, but don't rename db_table values or field names.
+from django.db import models
+
+
+class AuthGroup(models.Model):
+    name = models.CharField(unique=True, max_length=150)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_group'
+
+
+class AuthGroupPermissions(models.Model):
+    group = models.ForeignKey(AuthGroup, models.DO_NOTHING)
+    permission = models.ForeignKey('AuthPermission', models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_group_permissions'
+        unique_together = (('group', 'permission'),)
+
+
+class AuthPermission(models.Model):
+    name = models.CharField(max_length=255)
+    content_type = models.ForeignKey('DjangoContentType', models.DO_NOTHING)
+    codename = models.CharField(max_length=100)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_permission'
+        unique_together = (('content_type', 'codename'),)
+
+
+class AuthUser(models.Model):
+    password = models.CharField(max_length=128)
+    last_login = models.DateTimeField(blank=True, null=True)
+    is_superuser = models.IntegerField()
+    username = models.CharField(unique=True, max_length=150)
+    first_name = models.CharField(max_length=150)
+    last_name = models.CharField(max_length=150)
+    email = models.CharField(max_length=254)
+    is_staff = models.IntegerField()
+    is_active = models.IntegerField()
+    date_joined = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'auth_user'
+
+
+class AuthUserGroups(models.Model):
+    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
+    group = models.ForeignKey(AuthGroup, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_user_groups'
+        unique_together = (('user', 'group'),)
+
+
+class AuthUserUserPermissions(models.Model):
+    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
+    permission = models.ForeignKey(AuthPermission, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_user_user_permissions'
+        unique_together = (('user', 'permission'),)
+
+
+class AuthtokenToken(models.Model):
+    key = models.CharField(primary_key=True, max_length=40)
+    created = models.DateTimeField()
+    user = models.OneToOneField(AuthUser, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'authtoken_token'
+
+
+class DjangoAdminLog(models.Model):
+    action_time = models.DateTimeField()
+    object_id = models.TextField(blank=True, null=True)
+    object_repr = models.CharField(max_length=200)
+    action_flag = models.PositiveSmallIntegerField()
+    change_message = models.TextField()
+    content_type = models.ForeignKey('DjangoContentType', models.DO_NOTHING, blank=True, null=True)
+    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'django_admin_log'
+
+
+class DjangoContentType(models.Model):
+    app_label = models.CharField(max_length=100)
+    model = models.CharField(max_length=100)
+
+    class Meta:
+        managed = False
+        db_table = 'django_content_type'
+        unique_together = (('app_label', 'model'),)
+
+
+class DjangoMigrations(models.Model):
+    app = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
+    applied = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'django_migrations'
+
+
+class DjangoSession(models.Model):
+    session_key = models.CharField(primary_key=True, max_length=40)
+    session_data = models.TextField()
+    expire_date = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'django_session'
+
+
+class TblClub(models.Model):
+    name = models.CharField(max_length=45)
+    code = models.CharField(unique=True, max_length=6)
+    password = models.CharField(max_length=60)
+    profile_path = models.CharField(max_length=255, blank=True, null=True)
+    banner_path = models.CharField(max_length=255, blank=True, null=True)
+    contents = models.CharField(max_length=256, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'tbl_club'
+
+
+class TblClubTag(models.Model):
+    tag = models.ForeignKey('TblTag', models.DO_NOTHING)
+    club = models.ForeignKey(TblClub, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'tbl_club_tag'
+        unique_together = (('id', 'tag', 'club'),)
+
+
+class TblProjectIntroduction(models.Model):
+    title = models.CharField(max_length=50)
+    contents = models.TextField()
+    created_at = models.DateTimeField()
+    updated_at = models.DateTimeField(blank=True, null=True)
+    started_at = models.DateTimeField()
+    ended_at = models.DateTimeField(blank=True, null=True)
+    club = models.ForeignKey(TblClub, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'tbl_project_introduction'
+        unique_together = (('id', 'club'),)
+
+
+class TblProjectIntroductionImage(models.Model):
+    path = models.CharField(max_length=255)
+    project_introduction = models.ForeignKey(TblProjectIntroduction, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'tbl_project_introduction_image'
+        unique_together = (('id', 'project_introduction'),)
+
+
+class TblTag(models.Model):
+    tag_type = models.CharField(max_length=10)
+
+    class Meta:
+        managed = False
+        db_table = 'tbl_tag'
