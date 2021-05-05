8905bc25f75ec36ad2313cb7e04f41aa34c5d982
kwakdony <kwakdh25@gmail.com>
2021-05-05 19:08:11 +0900

    init

--- submit_pow/settings.py
+++ submit_pow/settings.py
@@ -23,12 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'powerof202!pow'
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'powerof202!pow')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool( os.environ.get("DJANGO DEBUG", True))
 
-ALLOWED_HOSTS = ['testserver', 'localhost', '127.0.0.1', 'floating-brook-63809.herokuapp.com']
+ALLOWED_HOSTS = ['testserver', '127.0.0.1', 'floating-brook-63809.herokuapp.com']
 
 
 # Application definition
