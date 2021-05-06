2c824a65b73b4b37ddd0f8c8f84378406047822a
kwakdony <kwakdh25@gmail.com>
2021-05-06 23:41:23 +0900

    FIX

--- submit_pow/settings.py
+++ submit_pow/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['testserver', 'localhost', '127.0.0.1', 'powerofpow.herokuapp.com']
 
