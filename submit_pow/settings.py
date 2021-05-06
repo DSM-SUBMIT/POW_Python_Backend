59d48ffab67b8862c620535be7a984c8f02240e6
kwakdony <kwakdh25@gmail.com>
2021-05-06 22:30:07 +0900

    CHANGE settings.py

--- submit_pow/settings.py
+++ submit_pow/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'powerof202!pow')
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = bool( os.environ.get("DJANGO DEBUG", True))
+DEBUG = bool(os.environ.get("DJANGO DEBUG", True))
 
 ALLOWED_HOSTS = ['testserver', 'localhost', '127.0.0.1', 'powerofpow.herokuapp.com']
 
