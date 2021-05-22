f86eacf6eafbe087dc7a4e8c84a72d869e498426
kwakdony <kwakdh25@gmail.com>
2021-05-22 22:10:33 +0900

    REFACTOR

--- club_project/views.py
+++ club_project/views.py
@@ -51,7 +51,7 @@ class ProjectAPIView(APIView):
 
         if serializer.is_valid():
             serializer.save()
-            return Response(serializer.data, status=201)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
         else:
             return Response(
                 serializer.errors,
