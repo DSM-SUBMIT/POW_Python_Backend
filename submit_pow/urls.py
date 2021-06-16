33a75041f0e7144b51e85b60fff4c5ceaf82e3f1
kwakdony <kwakdh25@gmail.com>
2021-06-16 19:12:30 +0900

    Bug fixed

--- submit_pow/urls.py
+++ submit_pow/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from club_project import views
+from submit_pow import views
 
 
 urlpatterns = [
